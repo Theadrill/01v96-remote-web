3b585db605354e24d476f89b04a1f3f932bb16c2
Rodrigo Vernaschi <theadrill@gmail.com>
2026-04-12 21:47:41 -0300

    chore: remove obsolete files from tmp directory

--- tmp/even_older/01v96-remote-web-main/public/app.js
+++
@@ -1,5 +0,0 @@
-// O app.js agora atua apenas como Bootstrapper final, 
-// assumindo que os scripts dos módulos em index.html já foram carregados sequencialmente.
-
-// Rodamos imediatamente para que os elementos existam antes do Socket inicial
-initUI();
