1a8796140560279c49aead052a7b35542d138a78
Theadrill <37183515+Theadrill@users.noreply.github.com>
2026-05-22 16:46:28 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+
+const os = require('os');
+os.networkInterfaces = () => {
+  return {
+    lo: [
+      {
+        address: '127.0.0.1',
+        netmask: '255.0.0.0',
+        family: 'IPv4',
+        mac: '00:00:00:00:00:00',
+        internal: true,
+        cidr: '127.0.0.1/8'
+      }
+    ]
+  };
+};
+
 // ============================================================================
 // server.js — Orquestrador Principal do 01V96 Bridge Server
 // ============================================================================
